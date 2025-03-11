18fed054a3e201bbfaad6781438d553ec0babe6f
Sarrah-1 <saraouaday1@gmail.com>
2025-03-11 01:56:55 +0000

    Mise à jour du code dans Test.java et Test.class

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,50 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package test;
 
-import beans.Etudiant;
 import beans.Livre;
+import beans.Etudiant;
 import beans.EmpruntLivre;
-import services.EtudiantService;
 import services.LivreService;
-import services.EmpruntService;
+import services.EtudiantService;
+import services.EmpruntService; 
 
-import java.time.LocalDate;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+/**
+ *
+ * @author hp
+ */
 public class Test {
-    public static void main(String[] args) {
-        EtudiantService etudiantService = new EtudiantService();
-        LivreService livreService = new LivreService();
+    public static void main(String[] args) throws ParseException {
+        LivreService ls = new LivreService();
+        EtudiantService es = new EtudiantService();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+        /*
+        ls.create(new Livre("Le Petit Prince", "Sara", "12222"));
+        ls.create(new Livre("1984", "hAJAR", "111111"));
+        
+        // Suppression d'un livre
+        ls.delete(ls.findById(1));
+        
+        // Mise à jour d'un livre
+        Livre livre = ls.findById(2);
+        livre.setTitre("Nineteen Eighty-Four");
+        livre.setAuteur("George Orwell");
+        ls.update(livre);
+        */
+
+        /*
+        es.create(new Etudiant("Ouaday", "Sara", "S.ouaday@gmail.com"));
+        es.create(new Etudiant("Saab", "Hajar", "H.saab@gmail.com"));
+        
+        es.delete(es.findById(1));
+        
+        Etudiant etudiant = es.findById(2);
+        etudiant.setNom("Amal");
+        etudiant.setPrenom("Raji");
+        etudiant.setEmail("amal@gmail.com");
+        es.update(etudiant);
+        */
+
+        /*
         EmpruntService empruntService = new EmpruntService();
-
-        // Tester le service des étudiants
-        Etudiant etudiant1 = new Etudiant("Ouaday", "Sara", "S.ouaday@gmail.com");
-        Etudiant etudiant2 = new Etudiant("Imani", "Rania", "R.imani@gmail.com");
+        Emprunt emprunt = new Emprunt(livre, etudiant, sdf.parse("15-03-2025")); // Assurez-vous que le livre et l'étudiant existent
+        empruntService.create(emprunt);
         
-        etudiantService.create(etudiant1);
-        etudiantService.create(etudiant2);
+        emprunt.setDateRetour(sdf.parse("30-03-2025"));
+        empruntService.update(emprunt);
+        */
         
-        // Mettre à jour un étudiant
-        etudiant1.setEmail("ouaday.sara@gmail.com");
-        etudiantService.update(etudiant1);
-
-        // Tester le service des livres
-        Livre livre1 = new Livre("Titre Livre 1", "Auteur 1", "Catégorie 1", true);
-        Livre livre2 = new Livre("Titre Livre 2", "Auteur 2", "Catégorie 2", true);
-        
-        livreService.create(livre1);
-        livreService.create(livre2);
-        
-        // Mettre à jour un livre
-        livre1.setTitre("Titre Livre Mis à Jour");
-        livreService.update(livre1);
-
-        // Tester le service des emprunts
-        EmpruntLivre emprunt1 = new EmpruntLivre(1, 1, LocalDate.now(), LocalDate.now().plusDays(30));
-        empruntService.create(emprunt1);
-
-        // Afficher tous les emprunts
-        System.out.println("Emprunts:");
-        for (EmpruntLivre emprunt : empruntService.findAll()) {
-            System.out.println("ID: " + emprunt.getId() + ", Livre ID: " + emprunt.getLivreId() + ", Étudiant ID: " + emprunt.getEtudiantId());
-        }
+        System.out.println("Teste exécutés avec succès !");
     }
 }

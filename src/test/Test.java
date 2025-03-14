e8953e72bbea54231b6cdf5f70ef40f7aee47bf0
Sarrah-1 <saraouaday1@gmail.com>
2025-03-14 21:25:19 +0000

    Ajout des modifications sur EmpruntService

--- src/test/Test.java
+++ src/test/Test.java
@@ -21,16 +21,18 @@ import java.text.SimpleDateFormat;
  * @author hp
  */
 public class Test {
+    private static Object sdf;
 
     public static void main(String[] args) throws ParseException {
         LivreService ls = new LivreService();
         EtudiantService es = new EtudiantService();
         EmpruntService ems = new EmpruntService();
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        /*
         es.create(new Etudiant("salma", "Sara", "S.salma@gmail.com"));
         es.create(new Etudiant("imane", "Hajaar", "H.imane@gmail.com"));
 
-        //es.delete(es.findById(2));
+        es.delete(es.findById(2));
         Etudiant e = es.findById(1);
         e.setNom("Amal");
         e.setPrenom("Raji");
@@ -60,26 +62,14 @@ public class Test {
         EStatut statut = EStatut.EMPRUNT;
         Etudiant etudiant = es.findById(1);
         Livre livre2 = ls.findById(1);
+*/
+         
+            ems.create(new EmpruntLivre(EStatut.EMPRUNT, sdf.parse("15-03-2025"), null, ls.findById(1), es.findById(1)));
 
-        if (etudiant != null && livre2 != null) {
-            EmpruntLivre emprunt = new EmpruntLivre(statut, sdf.parse("15-03-2025"), null, etudiant, livre);
-            ems.create(emprunt);
-            System.out.println("Emprunt enregistré pour le livre : " + livre2.getTitre());
+            System.out.println("Emprunt enregistré pour le livre : " + ls.findById(1).getTitre());
 
-            emprunt.setDateRetour(sdf.parse("30-03-2025"));
-            ems.update(emprunt);
-            System.out.println("Date de retour mise à jour : " + sdf.format(emprunt.getDateRetour()));
-
-            System.out.println("Liste des emprunts en base:");
-            for (EmpruntLivre e : ems.findAll()) {
-                String dateRetour = e.getDateRetour() != null ? sdf.format(e.getDateRetour()) : "Non retourné";
-                System.out.println("Livre: " + e.getLivreId()
-                        + " - Étudiant: " + e.getEtudiantId()
-                        + " - Date d'emprunt: " + sdf.format(e.getDateEmprunt())
-                        + " - Date de retour: " + dateRetour);
-            }
-
-            System.out.println("\n Tests terminés avec succès !");
+            
+        }
         }
     
-}
+    

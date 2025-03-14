074e187ef84fed3f4723a96942dc2b886bd31c6f
Sarrah-1 <saraouaday1@gmail.com>
2025-03-14 21:03:59 +0000

    Ajout des modifications sur EmpruntService

--- src/beans/EmpruntLivre.java
+++ src/beans/EmpruntLivre.java
@@ -3,19 +3,26 @@ package beans;
 import java.util.Date;
 
 public class EmpruntLivre {
+
     private EStatut statut;
     private Date dateEmprunt;
     private Date dateRetour;
     private Livre livre;
     private Etudiant etudiant;
 
-    public EmpruntLivre(EStatut statut, Date dateEmprunt, Date dateRetour,Livre livre, Etudiant etudiant ) {
+    public EmpruntLivre(EStatut statut, Date dateEmprunt, Date dateRetour, Livre livre, Etudiant etudiant) {
         this.statut = statut;
         this.dateEmprunt = dateEmprunt;
         this.dateRetour = dateRetour;
         this.livre = livre;
         this.etudiant = etudiant;
     }
+
+    public EmpruntLivre(int aInt, EStatut valueOf, java.sql.Date date, java.sql.Date date0, Livre livre, Etudiant etudiant) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
     public EStatut getStatut() {
         return statut;
     }
@@ -40,21 +47,18 @@ public class EmpruntLivre {
         this.dateRetour = dateRetour;
     }
 
-    public Livre getLivre() {
-        return livre;
-    }
-
-    public void setLivre(Livre Livre) {
-        this.livre = Livre;
-    }
-
     public Etudiant getEtudiant() {
         return etudiant;
     }
-    public int getLivreId() {
-    return livre != null ? livre.getId() : -1; // Retourne -1 si le livre est null
-    }
+
     public void setEtudiant(Etudiant etudiant) {
         this.etudiant = etudiant;
+    }
+     public Livre getLivre() {
+        return livre;
+    }
+
+    public void setLivre(Livre livre) {
+        this.livre = livre;
     }
 }
--- src/services/EmpruntService.java
+++ src/services/EmpruntService.java
@@ -1,105 +1,104 @@
 package services;
 
 import beans.EmpruntLivre;
+import beans.Etudiant;
+import beans.Livre;
 import connexion.Connexion;
 import dao.IDao;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import beans.EStatut;
+
 
 public class EmpruntService implements IDao<EmpruntLivre> {
-	private Connexion connexion;
-        private LivreService lvs;
-        private EtudiantService es;
-       
-	public EmpruntService() {
-		connexion = Connexion.getInstance();
-                lvs = new LivreService();
-                es = new EtudiantService();
+
+    private Connexion connexion;
+    private LivreService lvs;
+    private EtudiantService es;
+
+    public EmpruntService() {
+        connexion = Connexion.getInstance();
+        lvs = new LivreService();
+        es = new EtudiantService();
+
+    }
+
+    @Override
+    public boolean create(EmpruntLivre o) {
+        String req = "insert into emprunt_livre (statut, dateEmprunt, dateRetour, etudiant_id, livre_id) values (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = connexion.getCn().prepareStatement(req);
+            ps.setInt(1, o.getStatut().ordinal());
+            ps.setDate(2, new java.sql.Date(o.getDateEmprunt().getTime()));
+            ps.setDate(3, o.getDateRetour() != null ? new java.sql.Date(o.getDateRetour().getTime()) : null);
+            ps.setInt(4, o.getEtudiant().getId());
+            ps.setInt(5, o.getLivre().getId());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(EmpruntLivre o) {
+        String req = "delete from emprunt_livre where livre_id = ? AND etudiant_id = ?";
+        try {
+            PreparedStatement ps = connexion.getCn().prepareStatement(req);
+            ps.setInt(5, o.getLivre().getId());
+            ps.setInt(4, o.getEtudiant().getId());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(EmpruntLivre o) {
+        String req = "update emprunt_livre SET date_emprunt = ?, date_retour = ? where livre_id = ? AND etudiant_id = ?";
+        try {
+            PreparedStatement ps = connexion.getCn().prepareStatement(req);
+            ps.setDate(1, new java.sql.Date(o.getDateEmprunt().getTime()));
+            ps.setDate(2, new java.sql.Date(o.getDateRetour().getTime()));
+            ps.setInt(3, o.getLivre().getId());
+            ps.setInt(4, o.getEtudiant().getId());
+
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public EmpruntLivre findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<EmpruntLivre> findAll() {
+        List<EmpruntLivre> emprunts = new ArrayList<>();
+        String req = "select * from emprunt_livre";
+        try {
+            PreparedStatement ps = connexion.getCn().prepareStatement(req);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Livre livre = lvs.findById(rs.getInt("livre_id"));
                 
-	}
+                Etudiant etudiant = es.findById(rs.getInt("etudinat_id"));
 
-	@Override
-	public boolean create(EmpruntLivre o) {
-		String req = "insert into emprunt_livre (statut, dateEmprunt, dateRetour, etudiant_id, livre_id) values (?, ?, ?, ?, ?)";
-		try {
-			PreparedStatement ps = connexion.getCn().prepareStatement(req);
-                        ps.setInt(1, o.getStatut().ordinal());
-                        ps.setDate(2, new java.sql.Date(o.getDateEmprunt().getTime())); 
-                        ps.setDate(3, o.getDateRetour() != null ? new java.sql.Date(o.getDateRetour().getTime()) : null); 
-                        ps.setInt(4, o.getEtudiant());  
-                        ps.setInt(5, o.getLivreId());  
-        
-                        ps.executeUpdate(); 
-                        return true;
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-		return false;
-	}
-
-	@Override
-	public boolean delete(EmpruntLivre o) {
-		String req = "delete from emprunt_livre where id = ? AND etudiant_id = ?";
-		try {
-			PreparedStatement ps = connexion.getCn().prepareStatement(req);
-                        ps.setInt(2, o.getEtudiant().getId());
-			ps.executeUpdate();
-			return true;
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-		return false;
-	}
-
-	@Override
-	public boolean update(EmpruntLivre o) {
-		String req = "update emprunt_livre SET livre_id = ?, etudiant_id = ?, date_emprunt = ?, date_retour = ? where id = ?";
-		try {
-			PreparedStatement ps = connexion.getCn().prepareStatement(req);
-			ps.setInt(1, o.getLivreId());
-			ps.setInt(2, o.getEtudiantId());
-			ps.setDate(3, new java.sql.Date(o.getDateEmprunt().getTime()));
-                        ps.setDate(4, new java.sql.Date(o.getDateRetour().getTime()));
-			ps.setInt(5, o.getId());
-			ps.executeUpdate();
-			return true;
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-		return false;
-	}
-
-	@Override
-	public EmpruntLivre findById(int id) {
-		String req = "select * from emprunt_livre where id = ?";
-		try {
-			PreparedStatement ps = connexion.getCn().prepareStatement(req);
-			ps.setInt(1, id);
-			ResultSet rs = ps.executeQuery();
-			if (rs.next())
-				return new EmpruntLivre(rs.getInt("id"), rs.getInt("livre_id"), rs.getInt("etudiant_id"),
-                        rs.getDate("date_emprunt"), rs.getDate("date_retour"));
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-
-		return null;
-	}
-
-	@Override
-	public List<EmpruntLivre> findAll() {
-		List<EmpruntLivre> emprunts = new ArrayList<>();
-		String req = "select * from emprunt_livre";
-		try {
-			PreparedStatement ps = connexion.getCn().prepareStatement(req);
-			ResultSet rs = ps.executeQuery();
-                        while (rs.next()) {
-                        emprunts.add(new EmpruntLivre(rs.getInt("id"), rs.getInt("livre_id"), rs.getInt("etudiant_id"), rs.getDate("date_emprunt"), rs.getDate("date_retour")));
-                        }
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-		return emprunts;
-	}
+                emprunts.add(new EmpruntLivre(EStatut.valueOf(rs.getString("statut").toUpperCase()), rs.getDate("date_emprunt"), rs.getDate("date_retour"), livre, etudiant
+));
+             
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return emprunts;
+    }
 }
